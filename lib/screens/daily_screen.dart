6d33a22417c60ee23ea31be2fb2c7185731c984b
chrisD666 <chris.dias@lotusis.com>
2021-06-26 12:11:34 +0530

    transactions screen

--- lib/screens/daily_screen.dart
+++ lib/screens/daily_screen.dart
@@ -1,4 +1,5 @@
 import 'package:finance_manager/models/Tranasction_dates.dart';
+import 'package:finance_manager/models/Transaction.dart';
 import 'package:flutter/material.dart';
 
 class DailyScreen extends StatefulWidget {
@@ -10,7 +11,7 @@ class _DailyScreenState extends State<DailyScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey.shade50,
+      backgroundColor: Colors.grey.shade100,
       body: _Body(),
     );
   }
@@ -111,31 +112,121 @@ class __BodyState extends State<_Body> {
               ),
             ),
           ),
-          SizedBox(height: 30,),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          SizedBox(
+            height: 30,
+          ),
+          Column(
+            children: List.generate(transactions.length, (index) {
+              return Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Column(
                   children: [
-                    Container(
-                      width: (size.width) * 0.7,
-                      child: Row(children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
                         Container(
-                          width: 50,
-                          height: 50,
-                          decoration: BoxDecoration(
-
+                          // width: (size.width) * 0.7,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    color: Colors.grey.withOpacity(0.1),
+                                    shape: BoxShape.circle),
+                                child: Center(
+                                  child: Icon(
+                                    Icons.money,
+                                    size: 30,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Container(
+                                width: (size.width - 90) * 0.5,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      transactions[index].category,
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Text(
+                                      transactions[index].date.toString(),
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          color: Colors.black.withOpacity(0.5),
+                                          fontWeight: FontWeight.w400),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          width: (size.width - 40) * 0.3,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                "\$${transactions[index].amount}",
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.green),
+                              )
+                            ],
                           ),
                         )
-                      ],),
-                    )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 65, top: 8),
+                      child: Divider(
+                        thickness: 0.8,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
                   ],
+                ),
+              );
+            }),
+          ),
+          Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Row(children: [
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(right: 80),
+                  child: Text(
+                    "Total",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black.withOpacity(0.4)),
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  "\$1700.00",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black),
                 )
-              ],
-            ),
-          )
+              ]))
         ],
       ),
     );
